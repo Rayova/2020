d16c276c8a560055c32d5737a667c778568128b5
Rayova <55283119+Rayova@users.noreply.github.com>
2020-01-31 19:46:50 +0300

    ss

---
+++ 03/game.js
@@ -0,0 +1,36 @@
+console.log('hallo')
+// координаты
+let location1 = 5
+let location2 = 6
+let location3 = 7
+
+let currentShot
+
+let shots = 0
+
+let hits = 0
+
+let isSunk = false
+ console.log(2);
+while (isSunk === false) {
+   currentShot = Number(prompt('Введите число от 0 до 9:'))
+   if (currentShot > 9 || currentShot < 0) {
+     alert('число за пределами')
+   } else {
+      shots += 1
+   }
+   console.log(shots);
+   if (currentShot === location1 || currentShot === location2 || currentShot === location3) {
+     hits += 1
+   }
+
+   if (hits === 3) {
+     alert('Победа!')
+     isSunk = true
+    }
+   }
+   // Спаршиваю координату выстрела у игрока
+   // пишем цикл который работает пока корабль не утонул
+   // утонул ли корабль
+   // сколько выстрелов и попаданий
+   // переменная чтобы хранить координату выстрела
