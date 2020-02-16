21e66224a98c621fd712ddd2b2e7cdc40aef883f
Rayova <55283119+Rayova@users.noreply.github.com>
2020-02-16 18:40:54 +0300

    jj

--- anime/js/main.js
+++ anime/js/main.js
@@ -18,9 +18,48 @@ let propsAnim01 = anime({
 let propsBtn01 = document.getElementById('propsBtn01')
 propsBtn01.onclick = propsAnim01.play
 
+// let propsAnim01 = anime({
+//   targets: '.vals .triangle',
+//   translateX: 250,
+//   rotate: 180,
+//   easing: 'easeInOutQuad',
+//   delay: anime.stagger(100),
+//   autoplay: false,
+//   rotate: anime.stagger([-270, 270])
+// })
+// let propsBtn01 = document.getElementById('propsBtn01')
+// propsBtn01.onclick = propsAnim01.play
+//
+//
+// let propsAnim01 = anime({
+//   targets: '.vals .circle',
+//   translateX: 500,
+//   duration: 3000
+//   backgroundColor: 'rgba(234,152,62,0.5)'
+//   autoplay: false
+//   easing: 'spring(10, 90, 10, 5)'
+// })
+// let valsBtn01 = document.getElementById('propsBtn01')
+// propsBtn01.onclick = propsAnim01.play
+//
+//
+let propsBtn01 = document.getElementById('valsBtn01')
+valsBtn01.onclick = valssAnim01.play
 
+let path = anime.path('path')
+let svgMotion = anime({
+  targets: '.svgAnim .triangle',
+  translateX
 
+})
 
+let funcAnim01 = anime ({
+  targets: '.funk-move .box',
+  translateX: 200,
+  changeComplete: function(el) {
+    console.log('animation complete');
+  }
+})
 
 
 
